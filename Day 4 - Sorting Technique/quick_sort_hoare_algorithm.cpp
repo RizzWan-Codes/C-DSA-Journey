a3ef721ab9814e7109fe24b9749b661ae4991527
TarequeSyed <tareque1234syed@gmail.com>
2026-04-08 23:10:59 +0530

    Learnt Quick Sort in both algorithms!

---
+++ Day 4 - Sorting Technique/quick_sort_hoare_algorithm.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int Partition (vector<int>& arr, int left, int right) {
+    int pivot = arr[left];
+    int i = left - 1;
+    int j = right + 1;
+
+    while (true) {
+    do {
+        i++;
+    } while (arr[i] < pivot);
+
+    do {
+        j--;
+    } while (arr[j] > pivot);
+
+    if (i >= j) return j;
+
+    swap(arr[i], arr[j]);
+    }
+}
+
+void quick_sort (vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int Pi = Partition(arr, left, right);
+
+        quick_sort(arr, left, Pi);
+        quick_sort(arr, Pi + 1, right);
+    }
+}
+
+int main () {
+    vector<int> arr = {7, 14, 3, 1, 20, 5, 18, 9, 4, 15, 6, 2, 19, 8, 17, 10, 11, 13, 16, 12};
+
+    int left = 0;
+    int right = arr.size() - 1;
+
+    quick_sort(arr, left, right);
+
+    for (int x : arr) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
---
+++ Day 4 - Sorting Technique/quick_sort_lomuto-algorithm.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int Partition (vector<int>& arr, int left, int right) {
+    int pivot = arr[right];
+    int i = left - 1;
+
+    for (int j = left; j < right; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[right]);
+    return i + 1;
+}
+
+void quick_sort (vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int Pi = Partition(arr, left, right);
+        quick_sort(arr, left, Pi - 1);
+        quick_sort(arr, Pi + 1, right);
+    }
+}
+
+int main () {
+    vector<int> arr = {7, 14, 3, 1, 20, 5, 18, 9, 4, 15, 6, 2, 19, 8, 17, 10, 11, 13, 16, 12};
+
+    int left = 0;
+    int right = arr.size() - 1;
+
+    quick_sort(arr, left, right);
+
+    for (int x : arr) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
